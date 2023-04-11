5441ae93e055f949d229a266a0c7f18442833e23
Carissatasa <carissatasa12@gmail.com>
2023-04-11 22:04:17 +0700

    praktik-6

---
+++ linkcookies.php
@@ -0,0 +1,4 @@
+<?php
+	setcookie("variable_cookie","ini adalah variable cookie", time()+60);
+	echo "<a href=cekcookies.php>Cek Cookies</a>"
+?>
---
+++ redirect.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta http-equiv="refresh" content="2; url=http://localhost/praktik-6/test.php">
+</head>
+<body>
+	Tes Redirect. Setelah 2 detik, secara otomatis halaman ini akan dialihkan ke halaman tes.php 
+</body>
+</html>
